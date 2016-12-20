131d4a43a588f7e2cefff266ed90fda16a902647
Kimberly Lin <kimberlylin90@gmail.com>
2016-12-20 01:10:23 -0500

    calculate shipping

---
+++ CalculateShipping/src/calculateshipping/CalculateShipping.java
@@ -0,0 +1,39 @@
+/*
+ * Make a program to determine shipping costs
+ * Shipping rates:
+ * Over $75, free
+ * Between $50 and $75, cost is $5
+ * Over $25 but less than $50 is $15
+ */
+package calculateshipping;
+import java.util.Scanner;
+/**
+ *
+ * @author kimberlylin
+ */
+public class CalculateShipping {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double totalSale = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter total for the sale: ");
+        totalSale = in.nextDouble();
+        if (totalSale < 25) 
+        {
+            totalSale += 15;
+        }
+        else if (totalSale < 50)
+        {
+            totalSale += 10;
+        }
+        else if (totalSale < 75)
+        {
+            totalSale += 5;
+        }
+        System.out.println("Total cost for your sale is: " + totalSale);
+    }
+    
+}
