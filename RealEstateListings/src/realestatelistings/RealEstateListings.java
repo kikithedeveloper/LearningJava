18269ae66828b9b50b6dc4fbb18dc66b35c47842
Kimberly Lin <kimberlylin90@gmail.com>
2016-12-23 17:13:11 -0500

    instantiating objects

--- RealEstateListings/src/realestatelistings/RealEstateListings.java
+++ RealEstateListings/src/realestatelistings/RealEstateListings.java
@@ -15,7 +15,12 @@ public class RealEstateListings {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Property land = new Property(10000, "Land", 2.1);
+        Property estate = new Property(250000, "Estate", 1.5, 3, 4);
+        Property chi_property = new Property(25000, "Land", 12, 8, 26);
+        System.out.println(land.toString());
+        System.out.println(estate.toString());
+        System.out.println(chi_property.toString());
     }
     
 }
