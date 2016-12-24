d177ae770dceaaba3ddb0e6aaefb50133a29da5e
Kimberly Lin <kimberlylin90@gmail.com>
2016-12-24 02:51:15 -0500

    double[]array

---
+++ Heights/src/heights/Heights.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heights;
+import java.util.Scanner;
+/**
+ *
+ * @author kimberlylin
+ */
+public class Heights {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numStudents;
+        double[] heights;
+        System.out.println("How many students do you have? ");
+        numStudents = in.nextInt();
+        heights = new double[numStudents];
+        for (int i = 0; i < heights.length; i++)
+        {
+            System.out.println("Enter heights: ");
+            heights[i] = in.nextDouble();
+        }
+        double maxHeight = heights[0];
+        for (int i = 0; i < heights.length; i++)
+        {
+            if (maxHeight < heights[i])
+            {
+                maxHeight = heights[i];
+            }
+        }
+        double total = 0;
+        for (int i = 0; i < heights.length; i++)
+        {
+            total += heights[i];
+        }
+        System.out.println("The tallest student is: " + maxHeight + " inches");
+        System.out.println("The average height of all students is " + (total/numStudents) + " inches");
+    }
+    
+}
