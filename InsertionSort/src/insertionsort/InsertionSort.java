f3934a5a9c245f1aa7f638d5e72bf7ead3db6f22
Kimberly Lin <kimberlylin90@gmail.com>
2016-12-28 14:03:57 -0500

    clean up

--- InsertionSort/src/insertionsort/InsertionSort.java
+++ InsertionSort/src/insertionsort/InsertionSort.java
@@ -28,7 +28,7 @@ public class InsertionSort {
         for(j = 1; j < num.length; j++)
         {
             key = num[j];
-            for(i=j - 1; (i >= 0)&&(num[i] < key); i--)
+            for(i=j - 1; (i >= 0) && (num[i] > key); i--)
             {
                 num[i+1] = num[i];
             }
