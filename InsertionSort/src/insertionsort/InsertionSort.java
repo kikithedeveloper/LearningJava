a8d85a5b056d12a1e6c246cb9588e98d61ffd704
Kimberly Lin <kimberlylin90@gmail.com>
2016-12-27 20:06:36 -0500

    fix error

--- InsertionSort/src/insertionsort/InsertionSort.java
+++ InsertionSort/src/insertionsort/InsertionSort.java
@@ -25,15 +25,14 @@ public class InsertionSort {
     public static void InsertionSort(int[] num)
     {
         int j, key, i;
-        for(j = 1; j<num.length; j++)
+        for(j = 1; j < num.length; j++)
         {
             key = num[j];
-            for(i=j - 1; (i >= 0) && (num[i] < key); i--)
+            for(i=j - 1; (i >= 0)&&(num[i] < key); i--)
             {
                 num[i+1] = num[i];
             }
-            num[i+1] = num[i];
+            num[i+1] = key;
         }
     }
-    
 }
